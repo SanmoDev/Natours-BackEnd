32e5fa9aac337dac55923f0a7b5d01ed86175196
Ícaro Motta <icarosuperskate@gmail.com>
2021-02-15 09:07:22 -0300

    Rate Limiting Segurança dos headers melhorada Data sanitization Impedindo poluição de parametros

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const errorHandler = require('./controllers/error-controller');
 
 const app = express();
 
-//GLOBAL MIDDLEWARES
+//SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+//DEV LOGGING
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+//LIMIT REQUESTS FROM SAME IP
 const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000, //One hour
 	message:
 		'There were too many requests from your IP, please try again in an hour',
 });
+app.use('/api', limiter);
 
-app.use(express.json());
+//READ DATA FROM REQ.BODY
+app.use(express.json({limit: '10kb'}));
+
+//DATA SANITIZATION AGAINST NoSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+//PREVENT PARAMETER POLLUTION
+app.use(
+	hpp({
+		whitelist: [
+			'duration',
+			'ratingsQuantity',
+			'ratingsAverage',
+			'maxGroupSize',
+			'difficulty',
+			'price',
+		],
+	})
+);
+
+//SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
+
+//GET TIME OF REQUEST
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
@@ -26,7 +60,6 @@ const tourRouter = require('./routes/tour-routes');
 const userRouter = require('./routes/user-routes');
 const AppError = require('./Utils/AppError');
 
-app.use('/api', limiter);
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
