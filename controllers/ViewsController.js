de97fa73110d80a1411c08ca0b5a6942b2d3cd4e
Ícaro Motta <71195769+icarosuper@users.noreply.github.com>
2021-03-13 11:03:14 -0300

    Mapbox funcionando

--- controllers/ViewsController.js
+++ controllers/ViewsController.js
@@ -17,11 +17,17 @@ exports.getTour = globalCatch(async (req, res, next) => {
 		fields: 'review rating user',
 	});
 
-	if(!tour)
-		next(new AppError('Please input a correct tour name in the url', 400))
+	if (!tour)
+		next(new AppError('Please input a correct tour name in the url', 400));
 
-	res.status(200).render('tour', {
-		title: tour.name,
-		tour,
-	});
+	res
+		.status(200)
+		.set(
+			'Content-Security-Policy',
+			"default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+		)
+		.render('tour', {
+			title: tour.name,
+			tour,
+		});
 });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,50 @@
-console.log('apapa');
-// eslint-disable-next-line no-undef
-const locations = document.getElementById('map');
+/*eslint-disable*/
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiaWNhcm9tb3R0YSIsImEiOiJja2pyOW12bG4xOXMzMnNrM3NvMzVkM2JlIn0.n0LIYCjkhhmlsxXUD0ydYw';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/icaromotta/ckm7s8kj00jvf17qs1w7h8l72',
+	scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+	//CREATE MARKER
+	const el = document.createElement('div');
+	el.className = 'marker';
+
+	//ADD MARKER
+	new mapboxgl.Marker({
+		element: el,
+		anchor: 'bottom',
+	})
+		.setLngLat(loc.coordinates)
+		.addTo(map);
+
+	//POPUP
+	new mapboxgl.Popup({
+		offset: 30,
+	})
+		.setLngLat(loc.coordinates)
+		.setHTML(`<p>Day${loc.day}: ${loc.description}</p>`)
+		.addTo(map);
+
+	//EXTEND MAP TO FIT MARKERS
+	bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+	padding: {
+		top: 200,
+		bottom: 150,
+		left: 100,
+		right: 100,
+	},
+});
